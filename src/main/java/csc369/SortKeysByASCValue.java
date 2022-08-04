14651e3eaa2090b548836b04baa4c592fa2f7d68
Wesley Reynolds <42392208+WesReynolds@users.noreply.github.com>
2022-08-04 16:37:59 -0700

    Create SortKeysByASCValue.java

---
+++ src/main/java/csc369/SortKeysByASCValue.java
@@ -0,0 +1,45 @@
+package csc369;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SortKeysByASCValue {
+
+    public static final Class OUTPUT_KEY_CLASS = Text.class;
+    public static final Class OUTPUT_VALUE_CLASS = IntWritable.class;
+
+    public static class MapperImpl extends Mapper<LongWritable, Text, Text, IntWritable> {
+	private final IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+        @Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+	          word.set(itr.nextToken());  // ignore whitespace and punctuation
+            context.write(one, word);
+        }
+    }
+
+    public static class ReducerImpl extends Reducer<Text, IntWritable, Text, IntWritable> {
+	private IntWritable result = new IntWritable();
+    
+        @Override
+	protected void reduce(IntWritable count, Iterable<Text> words, Context context) throws IOException, InterruptedException {
+            Iterator<Text> itr = word.iterator();
+        
+            for (Text word : words) {
+              result.set(word);
+              context.write(count, word);
+            }
+            
+       }
+    }
+
+}
