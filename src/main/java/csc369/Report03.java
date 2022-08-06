698dd7136fbdbfe52aebab3adb909a453f4b1900
Wesley Reynolds <42392208+WesReynolds@users.noreply.github.com>
2022-08-06 12:15:34 -0700

    Create Report03.java

---
+++ src/main/java/csc369/Report03.java
@@ -0,0 +1,50 @@
+package csc369;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Report03 {
+
+    public static final Class OUTPUT_KEY_CLASS = Text.class;
+    public static final Class OUTPUT_VALUE_CLASS = IntWritable.class;
+
+    public static class MapperImpl extends Mapper<LongWritable, Text, Text, IntWritable> {
+	private final IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+        @Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    String[] tokens = value.toString().split(" ");
+		for (int i = 0; i < tokens.length; i++) {
+			if (i == tokens.length - 2) {
+				word.set(tokens[i]);	
+			}
+		}
+		
+            	context.write(word, one);
+        }
+    }
+
+    public static class ReducerImpl extends Reducer<Text, IntWritable, Text, IntWritable> {
+	private IntWritable result = new IntWritable();
+    
+        @Override
+	protected void reduce(Text word, Iterable<IntWritable> intOne, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            Iterator<IntWritable> itr = intOne.iterator();
+        
+            while (itr.hasNext()) {
+                sum  += itr.next().get();
+            }
+            result.set(sum);
+            context.write(word, result);
+       }
+    }
+}
