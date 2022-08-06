4202ff16b994dbc936a69833a8d27e1de0c1662f
Wesley Reynolds <42392208+WesReynolds@users.noreply.github.com>
2022-08-06 12:42:07 -0700

    Create Report04.java

---
+++ src/main/java/csc369/Report04.java
@@ -0,0 +1,55 @@
+package csc369;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Report04 {
+
+    public static final Class OUTPUT_KEY_CLASS = Text.class;
+    public static final Class OUTPUT_VALUE_CLASS = IntWritable.class;
+
+    public static class MapperImpl extends Mapper<LongWritable, Text, Text, IntWritable> {
+	private final IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+        @Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    		String targetAddress = "64.242.88.10";
+		word.set(targetAddress);
+		
+		IntWritable bytesSent;
+		String[] tokens = value.toString().split(" ");
+		if (tokens[0].equals(targetAddress)) {
+			bytesSent = new IntWritable(Integer.parseInt(tokens[tokens.length - 1]));	
+		}
+		else {
+			bytesSent = new IntWritable(0);	
+		}
+		
+            	context.write(word, bytesSent);
+        }
+    }
+
+    public static class ReducerImpl extends Reducer<Text, IntWritable, Text, IntWritable> {
+	private IntWritable result = new IntWritable();
+    
+        @Override
+	protected void reduce(Text word, Iterable<IntWritable> intOne, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            Iterator<IntWritable> itr = intOne.iterator();
+        
+            while (itr.hasNext()) {
+                sum  += itr.next().get();
+            }
+            result.set(sum);
+            context.write(word, result);
+       }
+    }
+}
